3d84d3254b95b8d3637b7c8a5f689f856010d3eb
Václav Pavlín <vaclav.pavlin@gmail.com>
2023-09-29 21:33:26 +0200

    add CSV output, push updates

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,19 @@
 
 WAKUCANARY=./bin/wakucanary
 
+TIME=$(date +%s)
 for node in `cat nodes.txt`; do
-    ${WAKUCANARY} -a=${node} -p=relay
-    echo $?
+    output=$(${WAKUCANARY} -a=${node} -p=relay)
+    success=$([ $? -eq 0 ] && echo true || echo false )
+    ping=$(echo ${output} | grep ping= | sed 's/.*ping=\([0-9]*\).*/\1/')
+    relay=$(echo ${output} | grep -q supported=/vac/waku/relay/2.0.0 && echo true || echo false)
+    #echo ${node} ${TIME} ${success} ${ping}ms ${relay}
+    echo "${node};${TIME};${success};${ping};${relay};" >> watched.csv
+    echo '{"node":"'${node}'", "ping": '${ping:-0}', "relay": '${relay}', "timestamp": '${TIME}', "success": '${success}'}' #| json_pp
 done
+
+git config --global user.name 'Waku Watchdog'
+git config --global user.email 'vpavlin@users.noreply.github.com'
+git add watched.csv
+git -m "watchdog run ${TIME}"
+git push
